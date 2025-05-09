2db73e2e60473e36e492a0a43ada30a43c1c3f35
arikmhm <mhm.ariyanto@gmail.com>
2025-05-09 14:11:40 +0700

    feat updae partners

--- src/data/partnersData.js
+++ src/data/partnersData.js
@@ -1,13 +1,15 @@
+import sponsor from "../assets/images/partners/sponsor.svg";
+import medpart from "../assets/images/partners/medpart.svg";
 export const sponsors = [
-  { name: "Sponsor A", logo: "/src/assets/images/lokasi.png" },
-  { name: "Sponsor B", logo: "/src/assets/images/lokasi.png" },
-  { name: "Sponsor C", logo: "/src/assets/images/lokasi.png" },
-  { name: "Sponsor D", logo: "/src/assets/images/lokasi.png" },
+  { name: "Sponsor A", logo: sponsor },
+  { name: "Sponsor B", logo: sponsor },
+  { name: "Sponsor C", logo: sponsor },
+  { name: "Sponsor D", logo: sponsor },
 ];
 
 export const mediaPartners = [
-  { name: "Media 1", logo: "/src/assets/images/lokasi.png" },
-  { name: "Media 2", logo: "/src/assets/images/lokasi.png" },
-  { name: "Media 3", logo: "/src/assets/images/lokasi.png" },
-  { name: "Media 4", logo: "/src/assets/images/lokasi.png" },
+  { name: "Media 1", logo: medpart },
+  { name: "Media 2", logo: medpart },
+  { name: "Media 3", logo: medpart },
+  { name: "Media 4", logo: medpart },
 ];
